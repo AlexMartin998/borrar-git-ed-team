82a15916f535ba389f94ce6b39a0a5b9de42c691
AlexMartin998 <alextravc.342@gmail.com>
2021-07-29 08:25:59 -0500

    Documentation updated

--- src/index.js
+++ src/index.js
@@ -14,4 +14,21 @@ const user = {
 }
 
 console.log(user);
-console.log('Hi');
+
+
+// // Classes
+class Person {
+	constructor(name, age = 12, so = 'Linux') {
+		this.name = name;
+		this.age = age;
+		this.so = so;
+	}
+
+	greeter() {
+		return `Hellow ${this.name}.`
+	}
+}
+
+const alex = new Person('Alex');
+console.log(alex);
+
